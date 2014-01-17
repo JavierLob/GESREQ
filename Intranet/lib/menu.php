e80075069e05cfb0926736308b7bffaa3b52a8b8
Javier Martín <recupera.javier@gmail.com>
2014-01-17 00:36:22 -0430

    corregido error del menú
    corregido error del menú

--- Intranet/lib/menu.php
+++ Intranet/lib/menu.php
@@ -1,21 +1,12 @@
 <?php	
 
-$uri 			= $_SERVER['REQUEST_URI'];
-$URL			= explode("/", $uri);
-//COMIENZO DE ETIQUETA PHP PARA ARMAR EL MENU 
-	//$MENU_LATERAL.= '<li>';
-	/*if($URL[2]=='app')
-	{
-
-	}*/
-
 	$MENU_LATERAL.= '<div class="page-sidebar nav-collapse collapse">';
 	$MENU_LATERAL.= '		<ul>';
 	$MENU_LATERAL.= '			<li>';
 	$MENU_LATERAL.= '				<div class="sidebar-toggler hidden-phone"></div>';
 	$MENU_LATERAL.= '			</li>';
 	$MENU_LATERAL.= '			<li class="active start ">';
-	$MENU_LATERAL.= '				<a href="../app">';
+	$MENU_LATERAL.= '				<a href="../app/">';
 	$MENU_LATERAL.= '				<i class="icon-home"></i> ';
 	$MENU_LATERAL.= '				<span class="title">Escritorio</span>';
 	$MENU_LATERAL.= '				<span class="selected"></span>';
