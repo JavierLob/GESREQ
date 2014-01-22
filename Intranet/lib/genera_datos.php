fb8fadebcd86cb82f8c4be6d05e0eef0b42447fa
Javier Martín <recupera.javier@gmail.com>
2014-01-21 22:39:42 -0430

    grafico de requerimientos
    grafico de requerimientos

--- Intranet/lib/genera_datos.php
+++ Intranet/lib/genera_datos.php
@@ -1,31 +1,33 @@
 <?php
 
-				$laRequerimientos=$lobjRequerimiento->listar_requerimientos();
-				for($i=0;$i<count($laRequerimientos);$i++)
-				{
-					$REQUERIMIENTOS.='new primitives.orgdiagram.ItemConfig({';
+                    $laRequerimientos=$lobjRequerimiento->listar_requerimientos();
+                    for($i=0;$i<count($laRequerimientos);$i++)
+                    {
+                         $REQUERIMIENTOS.='new primitives.orgdiagram.ItemConfig({';
                     $REQUERIMIENTOS.='id: '.$laRequerimientos[$i][0].',';
                     if($laRequerimientos[$i][10])
                     {
-                    	$REQUERIMIENTOS.='parent: '.$laRequerimientos[$i][10].',';
+                         $REQUERIMIENTOS.='parent: '.$laRequerimientos[$i][10].',';
                     }
                     else
                     {
-                    	$REQUERIMIENTOS.='parent: null,';
+                         $REQUERIMIENTOS.='parent: null,';
                     }                    
                     $REQUERIMIENTOS.='title: "'.$laRequerimientos[$i][2].'",';
-                    $REQUERIMIENTOS.='titleBackground: "red",';
+                    $REQUERIMIENTOS.='itemTitleColor:"#000012",';
                     $REQUERIMIENTOS.='description: "'.$laRequerimientos[$i][6].'",';
-                    $REQUERIMIENTOS.='image: "../media/img/foto/'.$laRequerimientos[$i][9].'.jpg"';
+                    $REQUERIMIENTOS.='label: "'.$laRequerimientos[$i][2].'",';
+                    $REQUERIMIENTOS.='image: "../media/img/foto/'.$laRequerimientos[$i][9].'.jpg",';
+                     $REQUERIMIENTOS.='templateName: "CursorTemplate"';
                     if($i==count($laRequerimientos)-1)
                     {
-						$REQUERIMIENTOS.='})';
-					}
-					else
-					{
-               			$REQUERIMIENTOS.='}),';
-					}	
-				}
+                              $REQUERIMIENTOS.='})';
+                         }
+                         else
+                         {
+                              $REQUERIMIENTOS.='}),';
+                         }    
+                    }
 
 
                
